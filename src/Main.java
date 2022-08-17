06bdc5d8a4b468ce932f69f2c64ff864d0c30535
Jane Volada <janevolada@gmail.com>
2022-08-17 14:20:39 +0300

    add files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 20_000;
+        int oneBonusMile = 20;
+        int numberOfBonusMiles = ticketPrice / oneBonusMile;
+        System.out.println(numberOfBonusMiles);
+    }
+}
